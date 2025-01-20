7ace2420a75bc73c68f6822c0b8d6087faec7137
Rafayel Latif <rafayel.latif@gmail.com>
2025-01-20 20:03:00 +0000

    update endpoints

--- src/routes/main.svelte
+++ src/routes/main.svelte
@@ -48,7 +48,7 @@
         isLoading = true
         
         const searchTerm = giftCategories[Math.floor(Math.random() * giftCategories.length)];
-        const productResponse = await fetch("http://127.0.0.1:5000/get-product", {
+        const productResponse = await fetch("https://gift-recsys.onrender.com/get-product", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ query: searchTerm, budget, agentInUse }),
