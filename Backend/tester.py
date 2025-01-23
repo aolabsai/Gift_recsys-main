990fa4fa9a5c08f4fde5db351c322982a829c20d
Rafayel Latif <rafayel.latif@gmail.com>
2025-01-23 20:35:32 +0000

    update tester .py

--- Backend/tester.py
+++ Backend/tester.py
@@ -1,14 +1,32 @@
-from dotenv import load_dotenv
-import os
-import http.client
 import requests
+import numpy as np
+import json
+#16+10+3
+url = "https://api.aolabs.ai/v0dev/kennel/agent"
 
-load_dotenv()
-
-rapid_key = os.getenv("RAPID_KEY")
-
-conn = http.client.HTTPSConnection("real-time-amazon-data.p.rapidapi.com")
-headers = {
-    'x-rapidapi-key': rapid_key,
-    'x-rapidapi-host': "real-time-amazon-data.p.rapidapi.com"
+LABEL = "0000000000"
+uid= "rafayel@aolabs.aitestingMum"
+payload = {
+    "kennel_id": "gift-recsys-1",  # use kennel_name entered above
+    "agent_id": uid,   # enter unique user IDs here, to call a unique agent for each ID
+    "request": "story",
 }
+
+headers = {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "X-API-KEY": "KzZbXbaahd1ElPO5Rtyv3a1ejHlw3Kn848c9SA1J"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+response_dict = json.loads(response.text)
+print(response_dict["state"])
+print(type(response_dict))
+
+print(response_dict.keys())
+story = response_dict["story"]
+story = list(story)
+
+story = np.asarray(story, dtype="int")
+print(story.shape)
+reshape = np.reshape(story, [277, 16+16+ 10+ 3])
