3e3900f84b542e0d2a12c35056e4f91b3a0f230b
Rafayel Latif <rafayel.latif@gmail.com>
2025-02-08 08:38:45 +0000

    get rid of slash

--- Backend/app.py
+++ Backend/app.py
@@ -29,7 +29,7 @@ import requests
 import time
 
 endpoint = "https://gift-recsys.onrender.com"  # change to https://gift-recsys.onrender.com for prod and http://127.0.0.1:5000 for local 
-frontend_url = "https://gift-recsys-main.onrender.com/"   #change to http://localhost:5174/ for local and  https://giftrec.aolabs.ai/ for prod
+frontend_url = "https://gift-recsys-main.onrender.com"   #change to http://localhost:5174 for local and  https://giftrec.aolabs.ai for prod
 
 app = Flask(__name__)
 app.secret_key = "super_secret_key"
