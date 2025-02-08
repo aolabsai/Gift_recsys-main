7237b131869091936bdeba1157757cc4b8517d17
spolisar <22416070+spolisar@users.noreply.github.com>
2025-02-07 20:41:10 -0500

    added build instructions for docker to readme.md, added git files to .dockerignore, address changes, gunicorn added back into requirements.txt

--- Backend/app.py
+++ Backend/app.py
@@ -29,11 +29,12 @@ import requests
 import time
 
 endpoint = "https://gift-recsys.onrender.com"  # change to https://gift-recsys.onrender.com for prod and http://127.0.0.1:5000 for local 
-frontend_url = "https://gift-recsys-main.onrender.com/"   #change to http://localhost:5174/ for local and  https://giftrec.aolabs.ai/ for prod
+# frontend_url = "https://gift-recsys-main.onrender.com/"   #change to http://localhost:5174/ for local and  https://giftrec.aolabs.ai/ for prod
+frontend_url = "http://localhost:5173/"
 
 app = Flask(__name__)
 app.secret_key = "super_secret_key"
-CORS(app, supports_credentials=True, origins=["http://localhost:5174"])
+CORS(app, supports_credentials=True, origins=["http://localhost:5173"])
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
--- src/routes/main.svelte
+++ src/routes/main.svelte
@@ -5,6 +5,7 @@
 
       
     const baseEndpoint = "https://gift-recsys.onrender.com"; // Change to http://127.0.0.1:5000 for local testing and https://gift-recsys.onrender.com for production
+    // const baseEndpoint = "http://127.0.0.1:5000"; 
 
     let countries = [];
     let selectedCountry = "US";
